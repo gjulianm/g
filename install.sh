853283dde6f73ee7bbf2b06919e0b1d8e4dcb913
Guillermo Julián <gjulian93@gmail.com>
2013-07-31 15:48:11 +0200

    Created install script

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+install_dir="$HOME/.g"
+
+install_to_rc () {
+	config_file="$1"
+	if [ -w $1 ]; then
+		echo "Installing to $config_file"
+		echo "source $install_dir/g.sh" >> $config_file
+		echo "source $install_dir/gcompletion.sh" >> $config_file
+	fi
+}
+
+if [ $# -eq 1 ]; then
+	install_dir="$1"
+elif [ $# -gt 1 ]; then
+	echo "usage: install.sh [install-dir]"
+	return
+fi
+
+mkdir -p $install_dir
+cp ./g.sh ./gcompletion.sh $install_dir
+echo "Created install directory $install_dir"
+
+install_to_rc $HOME/.bashrc
